a11dbeb2937c651dc239fe6949f27f9190375246
Adenaver <zhenyok@mail.ua>
2019-05-02 22:20:20 +0300

    Fix

--- index.js
+++ index.js
@@ -1,55 +1,38 @@
-function startGame()
-{
-	var time = document.getElementById('game-time').value
+function setTime(){
+	var time = document.getElementById("game-time").value
+	document.getElementById("time").innerHTML = time;
+}
+function startGame(){
+	var time = document.getElementById('game-time')
+	time.disabled=true;
 	document.getElementById('result').innerHTML = 0;
 	document.getElementById('result-header').style.display = 'none';
-	document.getElementById('time').innerHTML = time;
+	document.getElementById('time').innerHTML = time.value;
 	document.getElementById('start').style.display = 'none';
 	Game()
 }
 function Game(){
 	var prev = document.getElementById('time').innerHTML
-	if(prev!=0)
-	{
+	if(prev!=0){
 		document.getElementById('time').innerHTML = prev-1;
-		generator()
-		var timer = setTimeout(Game,1000)
+		setTimeout(Game,1000)
 	}
-	else
-	{
+	else{
+		resetPosition()
 		document.getElementById('result-header').style.display = 'block';
-		document.getElementById('error-message').style.display = 'none';
-		var div = document.getElementById('game');
-		while(div.firstChild){
-			div.removeChild(div.firstChild);
-		}
-		document.getElementById('start').style.display = 'block';	
+		document.getElementById('start').style.display = 'block';
+		document.getElementById('game-time').disabled=false;
 	}
 }
-function generator()
-{
-	if (document.getElementsByClassName('box').length<4)
-	{
-		document.getElementById('error-message').style.display = 'none';
-		var x=Math.random()*100;
-		x=Math.round(x);
-		var y=Math.random()*50;
-		y=Math.round(y);
-		var element = document.createElement("div");
-		element.onclick = function(e){
-			var points = parseInt(document.getElementById('result').innerHTML)
-			document.getElementById('result').innerHTML = points+1;
-			element.remove();
-		}
-		element.id="box"
-		element.className="box"
-		element.style.marginLeft=Math.random()*25+'px';
-		element.style.marginTop=Math.random()*250+'px';
-		element.borderColor="#fad64e"
-		document.getElementById('game').appendChild(element);
-	}
-	else
-	{
-		document.getElementById('error-message').style.display = 'block';
-	}
+function newPosition(){
+	var points = parseInt(document.getElementById('result').innerHTML)
+	document.getElementById('result').innerHTML = points+1;
+	var obj = document.getElementById('box')
+	obj.style.marginLeft=Math.random() * (-250 - (250))+250+'px';
+	obj.style.marginTop=Math.random()*250+'px';
+}
+function resetPosition(){
+	var obj = document.getElementById('box')
+	obj.style.marginLeft=0+"px";
+	obj.style.marginTop=125+"px";
 }

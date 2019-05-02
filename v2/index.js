743d178e6a55a60e9744f93a036b6b9d98c7e9d1
Adenaver <zhenyok@mail.ua>
2019-05-02 22:20:45 +0300

    Fix v2

---
+++ v2/index.js
@@ -0,0 +1,53 @@
+function setTime(){
+	var time = document.getElementById("game-time").value
+	document.getElementById("time").innerHTML = time;
+}
+function startGame(){
+	var time = document.getElementById('game-time').value
+	document.getElementById('result').innerHTML = 0;
+	document.getElementById('result-header').style.display = 'none';
+	document.getElementById('time').innerHTML = time;
+	document.getElementById('start').style.display = 'none';
+	Game()
+}
+function Game(){
+	var prev = document.getElementById('time').innerHTML
+	if(prev!=0)
+	{
+		document.getElementById('time').innerHTML = prev-1;
+		generator()
+		setTimeout(Game,1000)
+	}
+	else
+	{
+		document.getElementById('result-header').style.display = 'block';
+		document.getElementById('error-message').style.display = 'none';
+		var div = document.getElementById('game');
+		while(div.firstChild){
+			div.removeChild(div.firstChild);
+		}
+		document.getElementById('start').style.display = 'block';	
+	}
+}
+function generator(){
+	if (document.getElementsByClassName('box').length<4)
+	{
+		document.getElementById('error-message').style.display = 'none';
+		var element = document.createElement("div");
+		element.onclick = function(e){
+			var points = parseInt(document.getElementById('result').innerHTML)
+			document.getElementById('result').innerHTML = points+1;
+			element.remove();
+		}
+		element.id="box"
+		element.className="box"
+		element.style.marginLeft=Math.random()*25+'px';
+		element.style.marginTop=Math.random()*250+'px';
+		element.borderColor="#fad64e"
+		document.getElementById('game').appendChild(element);
+	}
+	else
+	{
+		document.getElementById('error-message').style.display = 'block';
+	}
+}
